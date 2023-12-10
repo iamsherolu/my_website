290fb41e1d062e775bc1378a0eba439e05784374
iamsherolu <152948449+iamsherolu@users.noreply.github.com>
2023-12-10 11:19:07 +0800

    20231210-1

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-$( ".website_title" ).textillate();
-$(".intro_content").textillate();
-function show_menu_items(){
+function show_menu_overlay(){
     document.getElementById("show_menu_overlay").style.display="block";
 }
-
 function off_menu_overlay(){
     document.getElementById("show_menu_overlay").style.display="none";
 }
@@ -26,3 +23,9 @@ window.addEventListener('resize', function () {
         off_menu_overlay();
     }
 });
+
+const text= document.querySelector('.circle_text p');
+text.innerHTML=text.innerText.split("").map(
+    (char,i)=>
+    `<span style="transform:rotate(${i*12}deg)">${char}</span> `   //讓circle_text上的文字都分別在一個<span>中
+).join("")
