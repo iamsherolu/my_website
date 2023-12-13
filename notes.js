9f82b3a8a39fa9d78a17e4c377ce8da4c1445600
iamsherolu <152948449+iamsherolu@users.noreply.github.com>
2023-12-13 15:12:54 +0800

    20231213-1

---
+++ notes.js
@@ -0,0 +1,25 @@
+function show_menu_overlay(){
+    document.getElementById("show_menu_overlay").style.display="block";
+}
+function off_menu_overlay(){
+    document.getElementById("show_menu_overlay").style.display="none";
+}
+function scrollToSection(sectionId) {
+    // 滾動到相應的位置
+    const section = document.getElementById(sectionId);
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    // 關閉overlay
+    const overlay = document.getElementById('show_menu_overlay');
+    if (overlay) {
+        overlay.style.display = 'none';
+    }
+}
+// 在螢幕大小變化時檢測並自動隱藏overlay
+window.addEventListener('resize', function () {
+    if (window.innerWidth >= 600) {
+        off_menu_overlay();
+    }
+});
