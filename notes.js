3a436c7d2388af5080b8f067c95e03eb9e7a02f2
iamsherolu <152948449+iamsherolu@users.noreply.github.com>
2024-01-08 00:00:45 +0800

    202401017-3

--- notes.js
+++ notes.js
@@ -1,3 +1,5 @@
+var overlay = document.getElementById('show_menu_overlay');
+
 function show_menu_overlay(){
     document.getElementById("show_menu_overlay").style.display="block";
 }
@@ -10,13 +12,13 @@ function scrollToSection(sectionId) {
     if (section) {
         section.scrollIntoView({ behavior: 'smooth' });
     }
-
-    // 關閉overlay
-    const overlay = document.getElementById('show_menu_overlay');
-    if (overlay) {
-        overlay.style.display = 'none';
-    }
 }
+
+function close_overlay(){
+    overlay.style.display='none';
+}
+
+
 // 在螢幕大小變化時檢測並自動隱藏overlay
 window.addEventListener('resize', function () {
     if (window.innerWidth >= 600) {
