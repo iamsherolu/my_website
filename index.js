4e6367a5538863d6c6b9fc924c5cc8e25bbf5d4e
iamsherolu <152948449+iamsherolu@users.noreply.github.com>
2024-01-26 16:07:02 +0800

    20240126-6

--- index.js
+++ index.js
@@ -32,7 +32,27 @@ text.innerHTML=text.innerText.split("").map(
 ).join("")
 
 
-// JavaScript 代碼用於實現圖片輪播
+//作品按按鈕切換
+let slideIndex = 1;
+showSlides(slideIndex);
 
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("gallery");
+  if (n > slides.length) {
+    slideIndex = 1
+}    
+  if (n < 1) {
+    slideIndex = slides.length
+}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "grid";  
+}
 
 
